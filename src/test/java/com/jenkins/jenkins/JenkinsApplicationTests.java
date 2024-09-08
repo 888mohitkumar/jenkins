af7a49d60709dac7c4461e72b6944e1a5670136b
888mohitkumar <888mohitkumar@gmail.com>
2024-09-08 16:45:35 +0530

    First commit

---
+++ src/test/java/com/jenkins/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jenkins.jenkins;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertThat(true);
+	}
+
+}
